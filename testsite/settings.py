4af0e4b0bd4fac2344e8d00a005ef18a847cb934
root <root@hw1.cloud.compas.cs.stonybrook.edu>
2018-02-28 19:12:05 +0000

    remove some

--- testsite/settings.py
+++ testsite/settings.py
@@ -31,8 +31,8 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEN = 'django.core.mail.backends.smtp.EmailBackend' 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'zilinggame'
-EMAIL_HOST_PASSWORD = 'zzl1997611'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Application definition
 
